dd51136b54dffbee26d404fd4de9ce54db1381b6
mstamenkov <bul2013@abv.bg>
2019-04-11 18:47:40 +0300

    added parktronic and day light code

--- second_module/second_module.ino
+++ second_module/second_module.ino
@@ -6,17 +6,130 @@ float R1 = 1000.0; // resistance of R1 (12K)
 float R2 = 600.0; // resistance of R2 (4.7K)
 int value = 0;
 int started = 0;
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+int trigPin_two = 9;    // Trigger
+int echoPin_two = 10;
+int trigPin_three = 4;    // Trigger
+int echoPin_three = 8;
+long duration, cm = 2000, inches;
+long cm_two;
+long cm_three;
 
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600);
-    pinMode(A0,INPUT);
+    pinMode(A0,INPUT); //voltmeter input
+    pinMode(5,OUTPUT); //signal for relay
+    digitalWrite(5,HIGH);
+    pinMode(3,INPUT); //signal from back sidelight
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(trigPin_two, OUTPUT);
+    pinMode(echoPin_two, INPUT);
+    pinMode(trigPin_three, OUTPUT);
+    pinMode(echoPin_three, INPUT);
+    pinMode(2, OUTPUT);
+    pinMode(13, INPUT);
 }
-
+ 
 void loop() {
   // put your main code here, to run repeatedly:
-    Serial.println(voltmeter());
-    delay(1000);
+    //Serial.println(voltmeter());
+    //delay(100);
+    if(voltmeter() > 13.2){
+      if(digitalRead(3) == LOW){
+        //Serial.println("gabariti OFF");
+        digitalWrite(5,LOW);
+        }
+      else digitalWrite(5,HIGH);
+      }
+      else digitalWrite(5,HIGH);
+
+      if(digitalRead(13) == HIGH){
+          //parktronik 1
+    
+          digitalWrite(trigPin, LOW);
+          delayMicroseconds(5);
+          digitalWrite(trigPin, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(trigPin, LOW);
+         
+          
+          pinMode(echoPin, INPUT);
+          duration = pulseIn(echoPin, HIGH);
+         
+          // Convert the time into a distance
+          //cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+    
+          Serial.print("one: ");
+          Serial.print(cm);
+          Serial.println("cm");
+    
+          //parktronik 2
+    
+          digitalWrite(trigPin_two, LOW);
+          delayMicroseconds(5);
+          digitalWrite(trigPin_two, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(trigPin_two, LOW);
+         
+          
+          pinMode(echoPin_two, INPUT);
+          duration = pulseIn(echoPin_two, HIGH);
+         
+          // Convert the time into a distance
+          cm_two = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+    
+          Serial.print("two: ");
+          Serial.print(cm_two);
+          Serial.println("cm");
+    
+          //parktronik 3
+    
+          digitalWrite(trigPin_three, LOW);
+          delayMicroseconds(5);
+          digitalWrite(trigPin_three, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(trigPin_three, LOW);
+         
+          
+          pinMode(echoPin_three, INPUT);
+          duration = pulseIn(echoPin_three, HIGH);
+         
+          // Convert the time into a distance
+          cm_three = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+    
+          
+          Serial.print("three: ");
+          Serial.print(cm_three);
+          delay(100);
+          Serial.println("cm");
+          
+          if((cm < 70 && cm > 50) || (cm_two < 70 && cm_two > 50) || (cm_three < 70 && cm_three > 50)){
+              tone(2, 4000, 200);
+              delay(200);
+              //noTone(2);
+          
+          }
+          else if((cm < 50 && cm > 30) || (cm_two < 50 && cm_two > 30) || (cm_three < 50 && cm_three > 30)){
+              tone(2, 4000, 200);
+              delay(100);
+              //noTone(2);
+          
+          }
+    
+          else if((cm < 30) || (cm_two < 30) || (cm_three < 30)){
+              tone(2, 4000);
+              //delay(50);
+              //noTone(2);
+          
+          }
+          else noTone(2);
+          //delay(250);
+
+      }
+      else noTone(2);
 }
 
 float voltmeter()
